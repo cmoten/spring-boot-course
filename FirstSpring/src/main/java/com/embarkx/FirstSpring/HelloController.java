0dc338c1922086240f15ba5aa7e89bf7e2641466
cmoten <cmoten@gmail.com>
2025-03-02 11:26:49 +0100

    reorganized files

--- FirstSpring/src/main/java/com/embarkx/FirstSpring/HelloController.java
+++ FirstSpring/src/main/java/com/embarkx/FirstSpring/HelloController.java
@@ -1,6 +1,11 @@
 package com.embarkx.FirstSpring;
 
-import org.springframework.web.bind.annotation.*;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
@@ -19,4 +24,5 @@ public class HelloController {
     public HelloResponse helloPost(@RequestBody String name) {
         return new HelloResponse("Hello, " + name + "!");
     }
+
 }
